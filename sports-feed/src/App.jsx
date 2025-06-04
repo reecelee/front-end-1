b9acf65b3a0a64d71bc7a82f29a80df6d3c4f8ba
Reece Lee <reecelee0128@gmail.com>
2025-06-03 19:01:27 -1000

    Map out some components that I want

--- sports-feed/src/App.jsx
+++ sports-feed/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <>
-    <h1>Hello World</h1>
+    <h1>Top Games of [Date]</h1>
+    <div>
+      <h2>Houston Astros(W) vs. Pittsburg Pirates(L) 3-0 </h2>
+      <p>The Houston Astros blanked the Pittsburg Pirates on their way to winning 3-0. Paul Skeenes, sophomore star, pitched 8 innings while only allowing one run. The opposing starter through 6 scoreless innings on his way to take home the win.</p>
+    </div>
+    <div>
+      <h2>Minnesota Lynx(W) vs. Pheonix Mercury(L) 88-65 </h2>
+      <p>The Minnesota Lynx continue their dominance of the WNBA with the Mercury as their latest victim. They improve to 8-0 on the season behind a strong game from Napheesa Collier scoring 18 points on 8/13 shooting. The Mercury were led by Lexi Hield who scored 16 points, but their defense couldn't stop the dominant Lynx.</p>
+    </div>
     </>
   )
 }
